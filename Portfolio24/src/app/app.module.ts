01a22200e3c4498fe344c387419753eac2b10aac
Sonia J Soacha <100891285+Argentina47@users.noreply.github.com>
2023-02-01 00:36:14 -0300

    Componentes

--- Portfolio24/src/app/app.module.ts
+++ Portfolio24/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    
   ],
   imports: [
     BrowserModule,
