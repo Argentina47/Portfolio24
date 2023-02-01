2b2c7a574ab0d60730e57fb160bbce3c0d979679
Sonia J Soacha <100891285+Argentina47@users.noreply.github.com>
2023-02-01 00:55:07 -0300

    componentes

--- Portfolio24/src/app/app.module.ts
+++ Portfolio24/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
@@ -14,6 +18,10 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     AcercaDeComponent,
     BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HomeComponent,
     
   ],
   imports: [
